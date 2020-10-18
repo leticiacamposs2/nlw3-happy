83d26d0540f72cf4cdde5e72c2f6fda89ca21d9e
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-10-17 21:00:32 -0300

    adicionando imagens no banco

--- backend/src/database/migrations/1602974250009-create_images.ts
+++ backend/src/database/migrations/1602974250009-create_images.ts
@@ -4,35 +4,35 @@ export class createImages1602974250009 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(new Table({
-            name: 'images',
-            columns: [
-                {
-                    name: 'id',
-                    type: 'integer',
-                    unsigned: true,
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: 'increment',
-                },
-                {
-                    name: 'path',
-                    type: 'varchar',
-                },
-                {
-                    name: 'orphanage_id',
-                    type: 'integer',
-                }
-            ],
-            foreignKeys: [
-                {
-                    name: 'ImageOrphanage',
-                    columnNames: ['orphanage_id'],
-                    referencedTableName: 'orphanage',
-                    referencedColumnNames: ['id'],
-                    onUpdate: 'CASCADE',
-                    onDelete: 'CASCADE',
-                }
-            ]
+          name: 'images',
+          columns: [
+            {
+              name: 'id',
+              type: 'integer',
+              unsigned: true,
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment',
+            },
+            {
+              name: 'path',
+              type: 'varchar',
+            },
+            {
+              name: 'orphanage_id',
+              type: 'integer',
+            },
+          ],
+          foreignKeys: [
+            {
+              name: 'ImageOrphanage',
+              columnNames: ['orphanage_id'],
+              referencedTableName: 'orphanages',
+              referencedColumnNames: ['id'],
+              onUpdate: 'CASCADE',
+              onDelete: 'CASCADE',
+            }
+          ],
         }))
     }
 
